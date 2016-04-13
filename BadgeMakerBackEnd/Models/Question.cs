207565fe418ec318acaf0c2586db8b4585ab4843
Martin Kearn <martin.kearn@microsoft.com>
2016-04-13 11:02:51 +0100

    Initial Model

---
+++ BadgeMakerBackEnd/Models/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BadgeMakerBackEnd.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string QuestionText { get; set; }
+
+        public List<QuestionOption> Options { get; set; }
+
+        public string Owner { get; set; }
+    }
+}
---
+++ BadgeMakerBackEnd/Models/QuizResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BadgeMakerBackEnd.Models
+{
+    public class QuizResponse
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public List<Answer> Answers { get; set; }
+    }
+}
